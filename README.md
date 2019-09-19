e1df63afe453ca773b38ab9b4edba08abbd21d3f
leopheard <leopheard.misc@gmail.com>
2019-09-19 03:07:52 -0400

    upload

---
+++ README.md
@@ -0,0 +1,13 @@
+RevolutionZ: Life After Capitalism Podcast<br>
+=============================
+
+A <a href="www.kodi.tv">KODI</a> / XBMC / Firestick audio addon for the <a href="https://www.zcomm.org/revolutionz">RevolutionZ: Life After Capitalism</a> podcast.<br>
+
+RevolutionZ: Life After Capitalism is a Podcast and YouTube Channel project, hosted by Michael Albert. It addresses timely events, media, and activism, emphasizing vision and strategy for a better future. It is all about what do we want and how do we get it?<br>
+
+<a href="www.kodi.tv">Kodi</a> (formerly known as XBMC) is an award-winning free and open source (GPL) software media player and entertainment hub that can be installed on Linux, OSX, Windows, iOS and Android, featuring a 10-foot user interface for use with televisions and remote controls. It allows users to play and view most videos, music, podcasts, and other digital media files from local and network storage media and the internet.<br>
+
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+
+<a href="http://www.zcomm.org/revolutionz"><img src="https://zcomm.org/wp-content/uploads/2019/05/RevolutionZ_3000-768x768.jpg"><br>
+<br><a href="http://www.kodi.tv"><img src="https://kodi.tv/sites/default/files/page/field_image/about--devices.jpg">
---
+++ resources/lib/mainaddon.py
@@ -0,0 +1,125 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def get_soup(url1):
+    """
+    @param: url of site to be scraped
+    """
+    page = requests.get(url1)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    print "type: ", type(soup)
+    return soup
+
+get_soup("https://feeds.buzzsprout.com/330161.rss")
+
+
+def get_playable_podcast1(soup):
+    """
+    @param: parsed html page            
+    """
+    subjects = []
+
+    for content in soup.find_all('item', limit=3):
+        
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print "\n\nLink: ", link
+
+            title = content.find('title')
+            title = title.get_text()
+
+#            desc = content.find('description')
+#            desc = desc.get_text()
+
+#            thumbnail = content.find('itunes:image')
+#            thumbnail = thumbnail.get('href')
+
+        except AttributeError:
+            continue
+              
+        item = {
+                'url': link,
+                'title': title,
+#                'desc': desc,
+                'thumbnail': "https://storage.buzzsprout.com/variants/g58P4VdAXD5G1Sgb4DSRRmCm/f81607a3cd537406cf0cf506c726bfe2824c5e584c9e9dc5e04e42436c820a79?.jpg"
+        }
+        
+        subjects.append(item) 
+    
+    return subjects
+
+
+def compile_playable_podcast1(playable_podcast1):
+    """
+    @para: list containing dict of key/values pairs for playable podcasts
+    """
+    items = []
+
+    for podcast in playable_podcast1:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+#            'info': podcast['desc'],
+            'is_playable': True,
+    })
+
+    return items
+
+
+def get_playable_podcast(soup):
+    """
+    @param: parsed html page            
+    """
+    subjects = []
+
+    for content in soup.find_all('item'):
+        
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print "\n\nLink: ", link
+
+            title = content.find('title')
+            title = title.get_text()
+
+#            desc = content.find('description')
+#            desc = desc.get_text()
+
+#            thumbnail = content.find('itunes:image')
+#            thumbnail = thumbnail.get('href')
+
+        except AttributeError:
+            continue
+              
+        item = {
+                'url': link,
+                'title': title,
+#                'desc': desc,
+                'thumbnail': "https://storage.buzzsprout.com/variants/g58P4VdAXD5G1Sgb4DSRRmCm/f81607a3cd537406cf0cf506c726bfe2824c5e584c9e9dc5e04e42436c820a79?.jpg",
+        }
+        
+        subjects.append(item) 
+    
+    return subjects
+
+
+def compile_playable_podcast(playable_podcast):
+    """
+    @para: list containing dict of key/values pairs for playable podcasts
+    """
+    items = []
+
+    for podcast in playable_podcast:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+#            'info': podcast['desc'],
+            'is_playable': True,
+    })
+
+    return items
